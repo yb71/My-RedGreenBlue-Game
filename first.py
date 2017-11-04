1c63464d57a4c8215eacc0183ba71cceb74a9175
Yulia Falls <yuliafalls@gmail.com>
2017-11-04 13:17:17 -0700

    commit

--- first.py
+++ first.py
@@ -1,9 +1,8 @@
 from tkinter import *
 from random import randint
 from enum import Enum
+import time
 # from tkinter.ttk import Frame, Label, Style, Button
-
-START = False
 
 
 class Direction(Enum):
@@ -11,9 +10,19 @@ class Direction(Enum):
 
 
 class Ball:
-    def __init__(self, canvas, size, fill="gray"):
+    def __init__(self, canvas, size):
         self.ball_size = size
         self.canvas = canvas
+
+        color = randint(1, 3)
+        if color == 1:
+            fill = "blue"
+        elif color == 2:
+            fill = "red"
+        elif color == 3:
+            fill = "green"
+        else:
+            fill = "gray"
 
         # randomize the starting point
         self.x1 = randint(50, 450)
@@ -33,6 +42,8 @@ class Ball:
             self.direction = Direction.RIGHT
 
     def move_ball(self):
+        global counter
+        counter += 1
         delta_x = randint(0, 3)
         delta_y = randint(0, 3)
         d = randint(0, 1)
@@ -50,7 +61,7 @@ class Ball:
         else: # Direction.RIGHT
             delta_y = d * delta_y  # randomize y direction
 
-        self.canvas.move(self.ball, delta_x, delta_y)
+        self.canvas.move(self.ball, delta_x, delta_y)  # -------------------------- move the ball
         self.canvas.after(40, self.move_ball)
 
         if len(canvas.coords(self.ball)) == 0:
@@ -59,17 +70,16 @@ class Ball:
             x1, y1, x2, y2 = canvas.coords(self.ball)
 
         overlapping_balls = canvas.find_overlapping(x1, y1, x2, y2)  # canvas detects overlapping
-        if len(overlapping_balls) > 1:
-            if START:
-                canvas.delete(self.ball)
-            else:
-                if self.direction == Direction.DOWN:
-                    self.direction = Direction.UP
-                elif self.direction == Direction.UP:
-                    self.direction = Direction.DOWN
-                elif self.direction == Direction.LEFT:
-                    self.direction = Direction.RIGHT
-                elif self.direction == Direction.RIGHT:
+        if len(overlapping_balls) > 1 and counter >= 30000:  # half a minute
+            canvas.delete(self.ball)
+        elif len(overlapping_balls) > 1 and counter < 30000:
+            if self.direction == Direction.DOWN:
+                self.direction = Direction.UP
+            elif self.direction == Direction.UP:
+                self.direction = Direction.DOWN
+            elif self.direction == Direction.LEFT:
+                self.direction = Direction.RIGHT
+            elif self.direction == Direction.RIGHT:
                     self.direction = Direction.LEFT
 
         # if hit the canvas walls
@@ -83,29 +93,43 @@ class Ball:
             self.direction = Direction.UP
 
 
-def callback():
-    START = True
+def update_clock():
+    start_time = START_TIME
+    diff = int(time.time() - start_time)
+    m, s = divmod(diff, 60)
+    h, m = divmod(m, 60)
+    now = "%d:%02d:%02d" % (h, m, s)
+    label.configure(text=now)
+    root.after(1000, update_clock)
 
 
+def count_survivors():
+    all_balls = canvas.find_all()
+    print(all_balls)
+    if len(all_balls) == 1:
+        exit()
+    root.after(1000, count_survivors)
+
+
+# ===========================================================================
 root = Tk()
-root.title('My window title')
+root.title('Red-Green-Blue')
 root.configure(background="gray")
 root.resizable(False, False)
+label = Label(text="", width=20, pady=5)
+label.pack(side=TOP)
 canvas = Canvas(root, bg='white', width=500, height=500)
-canvas.pack(side=TOP, padx=5, pady=5)
-button = Button(root, text="Start", width=20, pady=5, command=callback)
-button.pack(side=BOTTOM)
+canvas.pack(side=BOTTOM, padx=5, pady=5)
 
+START_TIME = time.time()
+update_clock()
+
+counter = 0
 for i in range(50):
     b = Ball(canvas, size=10)
     b.move_ball()
 
+
+count_survivors()
+
 root.mainloop()
-
-
-# center_x = self.x1 + int(self.ball_size / 2)
-# center_y = self.y1 + int(self.ball_size / 2)
-# item = canvas.find_closest(center_x, center_y)[0]
-# print(item)
-
-# canvas.itemconfig(self.ball, fill="red")
